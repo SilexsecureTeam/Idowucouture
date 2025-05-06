2b0b202f050e7ed82d6322e23f82f23b86ad38e6
Adeemma02 <emm.bolu@gmail.com>
2025-05-06 01:24:29 +0100

    dependency fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx'
-import { hot } from "react-hot-loader/root";
-
-const HotApp = hot(App);
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <StrictMode>
-  <HotApp />
+  <App />
   </StrictMode>
   </BrowserRouter>
 )
